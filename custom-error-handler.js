61c9e1b10fcba4d49399735b18cc811da4cca158
Doni Setiawan <donisetiawan.050@gmail.com>
2019-10-14 15:34:05 +0700

    Error handler

---
+++ custom-error-handler.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (request, response, next) => {
+  try {
+    throw new Error('Oh no!, something went wrong!');
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.use((error, request, response) => {
+  response.end(error.message);
+});
+
+app.listen(1337, () => console.log('Web Server running on port 1337'));
