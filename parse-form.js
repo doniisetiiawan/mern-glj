dcc9acdd789188ef719832422f654f3664521952
Doni Setiawan <donisetiawan.050@gmail.com>
2019-10-14 15:51:20 +0700

    HTTP request body

---
+++ parse-form.js
@@ -0,0 +1,39 @@
+/* eslint-disable no-tabs */
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+
+app.get('/', (request, response) => {
+  response.send(`
+	<!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>WebApp powered by ExpressJS</title>
+	</head>
+	<body>
+		<div role="application">
+			<form method="post" action="/setdata">
+				<input name="urlencoded" type="text" />
+				<button type="submit">Send</button>
+			</form>
+			<form method="post" action="/setdata" enctype="text/plain">
+				<input name="txtencoded" type="text" />
+				<button type="submit">Send</button>
+			</form>
+		</div>
+	</body>
+	</html>
+	`);
+});
+
+app.post('/setdata', (request, response) => {
+  console.log(request.body);
+  response.end();
+});
+
+app.listen(1337, () => console.log('Web Server running on port 1337'));
