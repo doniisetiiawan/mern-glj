47790e70bcf9aabfeb5ace74cde7ce414978cf0c
Doni Setiawan <donisetiawan.050@gmail.com>
2019-10-14 17:58:11 +0700

    Morgan logger

---
+++ morgan-logger.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+
+app.use(morgan('dev'));
+
+app.get('*', (request, response) => {
+  response.send('Hello Morgan!');
+});
+
+app.listen(1337, () => console.log('Web Server running on port 1337'));
