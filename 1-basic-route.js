08c47f795ab4957b8e8e9ba0e566a8c02696c957
Doni Setiawan <donisetiawan.050@gmail.com>
2019-10-14 14:28:03 +0700

    Routing in ExpressJS

---
+++ 1-basic-route.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (request, response) => {
+  response.status(200).send('Hello from ExpressJS');
+});
+
+app.listen(1337, () => console.log('Web Server running on port 1337'));
