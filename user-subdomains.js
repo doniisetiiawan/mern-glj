400db8b180dbd52710c90cb699003117adadd70d
Doni Setiawan <donisetiawan.050@gmail.com>
2019-10-14 18:14:42 +0700

    Vhost

---
+++ user-subdomains.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const vhost = require('vhost');
+
+const app = express();
+const users = express.Router();
+
+users.get('/', (request, response) => {
+  const username = request.vhost[0]
+    .split('-')
+    .map((name) => name[0].toUpperCase() + name.slice(1))
+    .join(' ');
+  response.send(`Hello, ${username}`);
+});
+
+app.use(vhost('*.localhost', users));
+
+app.listen(1337, () => console.log('Web Server running on port 1337'));
