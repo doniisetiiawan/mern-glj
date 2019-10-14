2464c3f99979f615c3f481b5145c7a3eb6a0725e
Doni Setiawan <donisetiawan.050@gmail.com>
2019-10-14 14:52:48 +0700

    Middleware

---
+++ middleware-functions.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app = express();
+
+app.use((request, response, next) => {
+  request.allowed = Reflect.has(request.query, 'allowme');
+  next();
+});
+
+app.get('/', (request, response) => {
+  if (request.allowed) {
+    response.send('Hello secret world!');
+  } else {
+    response.send('You are not allowed to enter');
+  }
+});
+
+app.listen(1337, () => console.log('Web Server running on port 1337'));
