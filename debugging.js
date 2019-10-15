f862fd581fc83d7393733a45c23830d29ac3c615
Doni Setiawan <donisetiawan.050@gmail.com>
2019-10-15 09:24:18 +0700

    Debugging

---
+++ debugging.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+app.get('*', (request, response) => {
+  response.send('Hello there!');
+});
+
+app.listen(1337, () => console.log('Web Server running on port 1337'));
---
+++ myapp.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+const debug = require('debug')('myapp');
+
+app.get('*', (request, response) => {
+  debug('Request:', request.originalUrl);
+  response.send('Hello there!');
+});
+
+app.listen(1337, () => console.log('Web Server running on port 1337'));
