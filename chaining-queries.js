4d57652695c91822598282913748d9b4e261daad
Doni Setiawan <donisetiawan.050@gmail.com>
2019-10-15 12:46:34 +0700

    Query builders

---
+++ chaining-queries.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+
+const { connection, Schema } = mongoose;
+
+mongoose.set('useUnifiedTopology', true);
+mongoose
+  .connect('mongodb://localhost:27017/test-xot', {
+    useNewUrlParser: true,
+  })
+  .catch(console.error);
+
+const UserSchema = new Schema({
+  firstName: String,
+  lastName: String,
+  age: Number,
+});
+
+const User = mongoose.model('User', UserSchema);
+
+connection.once('connected', async () => {
+  try {
+    const user = await new User({
+      firstName: 'John',
+      lastName: 'Snow',
+      age: 30,
+    }).save();
+
+    const findUser = await User.findOne()
+      .where('firstName')
+      .equals('John')
+      .where('age')
+      .lte(30)
+      .select('lastName age');
+
+    console.log(JSON.stringify(findUser, null, 4));
+
+    await user.remove();
+  } catch (error) {
+    console.dir(error.message, { colors: true });
+  } finally {
+    await connection.close();
+  }
+});
